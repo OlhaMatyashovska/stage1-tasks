649b3c6b3570a74e649b4a777663ffc9323f9f66
Olga Matyashovska <matyashowskamaria@gmail.com>
2021-03-13 19:40:09 +0200

    feat: implement load next picture functionality

--- photo-filter/app.js
+++ photo-filter/app.js
@@ -42,3 +42,43 @@ function resetFilters() {
            input.nextElementSibling.textContent = 0;
        }
   }
+  // load option --- punkt__4
+const nextButton = document.querySelector(".btn-next");
+const image = document.getElementById("image");
+let currentItem = 0;
+nextButton.addEventListener("click", switchPicture);
+window.addEventListener("DOMContentLoaded", function () {
+   let time = (new Date()).getHours();
+   let src = 'assets/images/';
+   if(time >= 6 && time < 12) {
+      src = src + 'morning/';
+   } else if(time >= 12 && time < 18){
+      src = src + 'day/'
+   } else if(time >= 18 && time < 24) {
+      src = src + 'evening/' 
+   } else {
+      src = src + 'night/';
+   }
+   image.src = src + '01.jpg';
+ });
+function switchPicture() {
+   let time = (new Date()).getHours();
+   let src = 'assets/images/';
+   let images_array = ["01.jpg","02.jpg","03.jpg","04.jpg","05.jpg","06.jpg","07.jpg","08.jpg","09.jpg","10.jpg",
+   "11.jpg","12.jpg","13.jpg","14.jpg","15.jpg","16.jpg","17.jpg","18.jpg","19.jpg","20.jpg"];
+   if(time >= 6 && time < 12) {
+      src = src + 'morning/';
+   } else if(time >= 12 && time < 18){
+      src = src + 'day/'
+   } else if(time >= 18 && time < 24) {
+      src = src + 'evening/' 
+   } else {
+      src = src + 'night/';
+   }
+   currentItem++;
+   if(currentItem > (images_array.length - 1)) {
+      currentItem = 0;
+   }
+   src = src + images_array[currentItem];
+   image.src=src;
+}
