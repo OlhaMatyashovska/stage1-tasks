01219750266aa78e69288746889630411731faff
Olga Matyashovska <matyashowskamaria@gmail.com>
2021-03-13 22:06:46 +0200

    feat: add basic download function implementation

--- photo-filter/app.js
+++ photo-filter/app.js
@@ -49,6 +49,7 @@ let currentItem = 0;
 nextButton.addEventListener("click", switchPicture);
 window.addEventListener("DOMContentLoaded", function () {
    let time = (new Date()).getHours();
+   //let src = 'https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/';
    let src = 'assets/images/';
    if(time >= 6 && time < 12) {
       src = src + 'morning/';
@@ -63,6 +64,7 @@ window.addEventListener("DOMContentLoaded", function () {
  });
 function switchPicture() {
    let time = (new Date()).getHours();
+   //let src = 'https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/';
    let src = 'assets/images/';
    let images_array = ["01.jpg","02.jpg","03.jpg","04.jpg","05.jpg","06.jpg","07.jpg","08.jpg","09.jpg","10.jpg",
    "11.jpg","12.jpg","13.jpg","14.jpg","15.jpg","16.jpg","17.jpg","18.jpg","19.jpg","20.jpg"];
@@ -93,3 +95,29 @@ function uploadImage(e) {
    }
    reader.readAsDataURL(file);
 }
+
+// download implementation
+const canvas = document.querySelector('canvas');
+// const ctx = canvas.getContext('2d');
+const download = document.querySelector('.btn-save');
+function drawImage() {
+   //const img = new Image();  
+   //image.setAttribute('crossOrigin', 'anonymous');
+   //img.src = image.src;
+   //img.onload = function() {
+     canvas.width = image.width;
+     canvas.height = image.height;
+     const ctx = canvas.getContext("2d");
+     ctx.drawImage(image, 0, 0);
+  // }; 
+ }
+ drawImage();
+
+ download.addEventListener('click', function(e) {
+   console.log(canvas.toDataURL());
+   var link = document.createElement('a');
+   link.download = 'download.png';
+   link.href = canvas.toDataURL();
+   link.click();
+   link.delete;
+ });
