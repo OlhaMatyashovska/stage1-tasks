980cde9163e8b1614d8b96480f01d887a46460ba
Olga Matyashovska <matyashowskamaria@gmail.com>
2021-03-13 15:43:17 +0200

    feat: punkt_1, punkt_2

--- photo-filter/app.js
+++ photo-filter/app.js
@@ -5,6 +5,8 @@ filters.forEach(input => input.addEventListener("input", applyFilters));
 //outputs.forEach(output => output.addEventListener("input", setCorrectData));
 function applyFilters() {
    const suffix = this.dataset.sizing || '';
+   // не дуже розумію як працює setProperty and Css variables
    document.documentElement.style.setProperty(`--${this.name}`, this.value+suffix);
+   event.target.nextElementSibling.value = this.value;
 }
 
