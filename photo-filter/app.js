831616505284f92d7f8692d5fb0c0ac712ad4742
Olga Matyashovska <matyashowskamaria@gmail.com>
2021-04-24 20:14:23 +0300

    feat: implement download functionlity without css filters + canvas styling

--- photo-filter/app.js
+++ photo-filter/app.js
@@ -49,8 +49,8 @@ let currentItem = 0;
 nextButton.addEventListener("click", switchPicture);
 window.addEventListener("DOMContentLoaded", function () {
    let time = (new Date()).getHours();
-   //let src = 'https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/';
-   let src = 'assets/images/';
+   let src = 'https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/';
+   //let src = 'assets/images/';
    if(time >= 6 && time < 12) {
       src = src + 'morning/';
    } else if(time >= 12 && time < 18){
@@ -61,11 +61,13 @@ window.addEventListener("DOMContentLoaded", function () {
       src = src + 'night/';
    }
    image.src = src + '01.jpg';
+   drawImage(image.src);
  });
+
 function switchPicture() {
    let time = (new Date()).getHours();
-   //let src = 'https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/';
-   let src = 'assets/images/';
+   let src = 'https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/';
+   //let src = 'assets/images/';
    let images_array = ["01.jpg","02.jpg","03.jpg","04.jpg","05.jpg","06.jpg","07.jpg","08.jpg","09.jpg","10.jpg",
    "11.jpg","12.jpg","13.jpg","14.jpg","15.jpg","16.jpg","17.jpg","18.jpg","19.jpg","20.jpg"];
    if(time >= 6 && time < 12) {
@@ -83,38 +85,37 @@ function switchPicture() {
    }
    src = src + images_array[currentItem];
    image.src=src;
+   drawImage(src);
 }
 // load image implementation
 const inputFileButton = document.getElementById("btnInput");
+
 inputFileButton.addEventListener("change",uploadImage);
 function uploadImage(e) {
    const file = inputFileButton.files[0];
    const reader = new FileReader();
    reader.onload = () => {
       image.src = reader.result;
+      drawImage(reader.result);
    }
    reader.readAsDataURL(file);
 }
-
 // download implementation
 const canvas = document.querySelector('canvas');
-// const ctx = canvas.getContext('2d');
-const download = document.querySelector('.btn-save');
-function drawImage() {
-   //const img = new Image();  
-   //image.setAttribute('crossOrigin', 'anonymous');
-   //img.src = image.src;
-   //img.onload = function() {
-     canvas.width = image.width;
-     canvas.height = image.height;
-     const ctx = canvas.getContext("2d");
-     ctx.drawImage(image, 0, 0);
-  // }; 
- }
- drawImage();
+const ctx = canvas.getContext("2d");
 
- download.addEventListener('click', function(e) {
-   console.log(canvas.toDataURL());
+function drawImage(src) {
+  const img = new Image();  
+  img.setAttribute('crossOrigin', 'anonymous');
+  img.src = src;
+  img.onload = function() {
+    canvas.width = img.width;
+    canvas.height = img.height;
+    ctx.drawImage(img, 0, 0);
+  }; 
+}
+const saveButton = document.querySelector(".btn-save");
+saveButton.addEventListener('click', function(e) {
    var link = document.createElement('a');
    link.download = 'download.png';
    link.href = canvas.toDataURL();
